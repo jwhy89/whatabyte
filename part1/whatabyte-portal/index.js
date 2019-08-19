23c41f258e2e95fed1b3a1a81c29d562ed62fba0
Jarvis Yang <jarvisgyang@gmail.com>
2019-08-19 16:49:32 -0500

    setup passport.js with node and express

--- part1/whatabyte-portal/index.js
+++ part1/whatabyte-portal/index.js
@@ -6,6 +6,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || "8000";
 
+const expressSession = require("express-session");
+
+// App instance, port declaration
+
+const session = {
+  secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  session.cookie.secure = true; // Serve secure cookies, requires HTTPS
+}
+
+app.use(expressSession(session));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
