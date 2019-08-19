ccf62245851bef5a90d6d651298158b1efe8f516
Jarvis Yang <jarvisgyang@gmail.com>
2019-08-19 16:57:57 -0500

    added auth.js Express Authentication Endpoints; added Auth0 strategy to index.js

--- part1/whatabyte-portal/index.js
+++ part1/whatabyte-portal/index.js
@@ -1,5 +1,7 @@
 // index.js
 
+require("dotenv").config();
+
 const path = require("path");
 const express = require("express");
 
@@ -21,6 +23,9 @@ if (app.get("env") === "production") {
   session.cookie.secure = true; // Serve secure cookies, requires HTTPS
 }
 
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
 app.use(expressSession(session));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -32,6 +37,45 @@ app.get("/", (req, res) => {
 
 app.get("/user", (req, res) => {
   res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+});
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API 
+     * (resource server)
+     * accessToken is the token to call the Auth0 API 
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
 });
 
 app.listen(port, () => {
